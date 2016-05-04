62ed56fa6f882b99d3eec0715fa907e8e2ceb4dd
Priyank P Shah <priyankp.shah@ishisystems.com>
2016-05-04 06:49:17 +0530

    Optimized pom.xml

--- src/test/java/com/test/RunCukesTest.java
+++ src/test/java/com/test/RunCukesTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 import java.io.File;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "json:cucumber.json", features = {
+@CucumberOptions(plugin = "json:target/cucumber.json", features = {
         "src/test/resources/features/Notification.feature",
         "src/test/resources/features/SecureDialog.feature"},
         glue = "com.test.stepdefs")
